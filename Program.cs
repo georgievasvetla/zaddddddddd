3f86ca945bb5e042a895b2d243b053244bda4caa
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-02-22 21:07:03 +0200

    ffff

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace zadNok
+{
+    internal class Program
+    {
+        static int NOD(int a,int b)
+        {
+            return b == 0 ? a : NOD(b, a % b);
+        }
+        static int NOK(int a,int b)
+        {
+            return a * (b / NOD(a, b));
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int nok=NOK(a,b);
+            Console.WriteLine(nok);
+        }
+    }
+}
